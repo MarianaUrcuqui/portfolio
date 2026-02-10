ab2ab7adc25d7fb05c31aa48c8d8856750bfb1f1
Mariana <mariana.urcuqui01@gmail.com>
2026-02-10 09:04:10 +0100

    deleted second project

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Layout from './components/Layout'
-import About from './pages/About'
-import LayoutProjects from './components/LayoutProjects'
-import Project01 from './pages/Project01'
-import Project02 from './pages/Project02'
-import Project03 from './pages/Project03'
-
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Layout from "./components/Layout";
+import About from "./pages/About";
+import LayoutProjects from "./components/LayoutProjects";
+import Project01 from "./pages/Project01";
+import Project03 from "./pages/Project03";
 
 function App() {
-
   return (
     <>
       <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route index element={<Home />}/>
-          <Route path='about' element={<About />}/>
-          <Route path='projects' element={<LayoutProjects />}>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="projects" element={<LayoutProjects />}>
             <Route index element={<Project01 />} />
-            <Route path='project02' element={<Project02 />}/>
-            <Route path='project03' element={<Project03 />}/>
+            <Route path="project03" element={<Project03 />} />
           </Route>
         </Route>
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/ProjectsNav.js
+++ src/components/ProjectsNav.js
@@ -1,26 +1,30 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 function ProjectsNav() {
   return (
-    <div className='projects-nav-div'>
-    <nav className='nav'>
-    <NavLink
-      to='.'
-      end
-      style={({ isActive }) => isActive ?   {textDecoration: 'underline'} : null}
-    >Project 1</NavLink>
-    <NavLink 
-      to='project02'
-      style={({ isActive }) => isActive ?   {textDecoration: 'underline'} : null}
-    >Project 2</NavLink>
-    <NavLink 
-      to='project03'
-      style={({ isActive }) => isActive ?   {textDecoration: 'underline'} : null}
-    >Project 3</NavLink>
-    </nav>
-  </div>
-  )
+    <div className="projects-nav-div">
+      <nav className="nav">
+        <NavLink
+          to="."
+          end
+          style={({ isActive }) =>
+            isActive ? { textDecoration: "underline" } : null
+          }
+        >
+          Project 1
+        </NavLink>
+        <NavLink
+          to="project03"
+          style={({ isActive }) =>
+            isActive ? { textDecoration: "underline" } : null
+          }
+        >
+          Project 2
+        </NavLink>
+      </nav>
+    </div>
+  );
 }
 
-export default ProjectsNav
+export default ProjectsNav;
